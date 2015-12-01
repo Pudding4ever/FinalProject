e9dd22af034368cacd8b608cfc593d74393dc7f6
SonarBoy <Joshua.L99.C@gmail.com>
2015-12-01 15:14:17 -0500

    Basic enemy float

---
+++ New Unity Project/Assets/SampleAgent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgent : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+	
+	void Start () {
+        this.agent = this.gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        agent.SetDestination(target.position);
+    }
+}
